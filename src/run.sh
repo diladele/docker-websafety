4e28b58b16e75c78ba46864b8fd5a4a8af7177c7
Rafael Akchurin <rafael.akchurin@diladele.com>
2016-04-20 22:55:35 +0200

    remove comments

--- src/run.sh
+++ src/run.sh
@@ -1,12 +1,4 @@
-DIR=`pwd`
 docker run \
     --name websafety \
     -p 80:80 -p 8080:3128 \
     -t diladele-websafety:4.4.0
-
-#    -v $DIR/../websafety-data/etc:/opt/qlproxy/etc:ro \
-#    -v $DIR/../websafety-data/var/spool:/opt/qlproxy/var/spool:ro \
-#    -v $DIR/../websafety-data/var/log:/opt/qlproxy/var/log \
-#    -v $DIR/../websafety-data/var/monitor:/opt/qlproxy/var/monitor \
-#    -v $DIR/../websafety-data/var/temp:/opt/qlproxy/var/temp \
-#
