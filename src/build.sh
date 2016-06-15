01395fb4cedd5d2a7ae6b3b8eef5531d25183404
Rafael Akchurin <rafael.akchurin@diladele.com>
2016-06-15 13:15:07 +0200

    switched tag

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,3 @@
-#docker rmi -f diladele-websafety:4.4.0
-#docker build --force-rm --no-cache --rm=true -t diladele-websafety:4.4.0 .
-docker build -t diladele-websafety:4.4.0 .
+docker rmi -f diladele-websafety:4.5.0
+docker build --force-rm --no-cache --rm=true -t diladele-websafety:4.5.0 .
+docker build -t diladele-websafety:4.5.0 .
