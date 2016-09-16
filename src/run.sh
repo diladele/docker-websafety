69c28f5c0b3f3d83cb8b10c9d67332176a062523
Ian Bashford <ian@bashford.net>
2016-09-16 22:35:37 +0100

    fix issue with updating defn files

--- src/run.sh
+++ src/run.sh
@@ -1,2 +1,2 @@
-docker run -it --name websafety-config -t websafety-root  /opt/qlproxy/bin/certmgr -action="regenerate-certificate-storage"
+docker run -it --name websafety-config -t websafety-root  /usr/local/bin/firstrun.sh
 docker run -d --name websafety-runtime --volumes-from websafety-config -p 80:80 -p 8080:3128 -t websafety-root
