9bc0e7e775ca0a8f3910b0027d7f14f941bfdbe4
Diladele <support@diladele.com>
2016-10-16 22:50:17 +0200

    Fixing container name

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,3 @@
-docker rm -f websafety-config websafety-runtime
+docker rm -f websafety-config websafety
 docker rmi -f diladele/websafety:4.7-beta
 docker build --force-rm --no-cache --rm=true -t diladele/websafety:4.7-beta .
