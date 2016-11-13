9ebf1e74c4a7a335b48c2bc84dc731124948a109
Diladele <support@diladele.com>
2016-11-13 21:19:52 +0100

    Switching to 4.7 release

--- src/run.sh
+++ src/run.sh
@@ -1,2 +1,2 @@
-docker run -it --name websafety-config diladele/websafety:4.7-beta /usr/local/bin/firstrun.sh
-docker run -d --name websafety --volumes-from websafety-config -p 8000:80 -p 3128:3128 -t diladele/websafety:4.7-beta
+docker run -it --name websafety-config diladele/websafety:4.7 /usr/local/bin/firstrun.sh
+docker run -d --name websafety --volumes-from websafety-config -p 8000:80 -p 3128:3128 -t diladele/websafety:4.7
