908e313686bb25d41472058f5edf9ff5669b72d3
Rafael Akchurin <rafael.akchurin@diladele.com>
2018-12-13 21:50:05 +0100

    redone docker for version 7

--- src/build.sh
+++ src/build.sh
@@ -1,10 +1,10 @@
 # delete all containers
-docker rm -f `docker ps -a -q`
+# docker rm -f `docker ps -a -q`
 
 # delete all images
-docker rmi -f `docker images -q`
+# docker rmi -f `docker images -q`
 
 # rebuild our project
 docker rm -f websafety-config websafety
-docker rmi -f diladele/websafety:6.4
-docker build --force-rm --no-cache --rm=true -t diladele/websafety:6.4 .
+docker rmi -f diladele/websafety:7.0
+docker build --force-rm --no-cache --rm=true -t diladele/websafety:7.0 .
