b35a9482896b00ec1e934b72ac301b774263692d
Diladele <support@diladele.com>
2020-12-30 16:27:50 +0100

    Switching to version 7.5

--- src/build.sh
+++ src/build.sh
@@ -1,10 +1,3 @@
-# delete all containers
-docker rm -f `docker ps -a -q`
-
-# delete all images
-docker rmi -f `docker images -q`
-
 # rebuild our project
-docker rm -f websafety-config websafety
-docker rmi -f diladele/websafety:7.0
-docker build --force-rm --no-cache --rm=true -t diladele/websafety:7.0 .
+docker rmi -f diladele/websafety:7.5
+docker build --force-rm --no-cache --rm=true -t diladele/websafety:7.5 .
--- src/run.sh
+++ src/run.sh
@@ -1,2 +1,1 @@
-docker run -it --name websafety-config diladele/websafety:7.0 /usr/local/bin/firstrun.sh
-docker run -dt --name websafety --dns=8.8.8.8 --volumes-from websafety-config -p 8000:80 -p 3128:3128 diladele/websafety:7.0
+docker run -dt --name websafety --dns=8.8.8.8 -p 8000:80 -p 3128:3128 diladele/websafety:7.5
