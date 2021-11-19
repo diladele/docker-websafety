1daec267c4e0a5706622797b6ca3c15d76f0c935
Rafael Akchurin <rafael.akchurin@diladele.com>
2021-11-19 10:29:55 +0100

    8.0

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,3 @@
 # rebuild our project
-docker rmi -f diladele/websafety:7.6
-docker build --force-rm --no-cache --rm=true -t diladele/websafety:7.6 .
+docker rmi -f diladele/websafety:8.0
+docker build --force-rm --no-cache --rm=true -t diladele/websafety:8.0 .
